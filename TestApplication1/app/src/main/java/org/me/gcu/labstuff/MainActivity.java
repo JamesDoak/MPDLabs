ed35fbe3464b3f8328602f3106613b10cd54bd73
jdoak200@caledonian.ac.uk <jdoak200@caledonian.ac.uk>
2022-03-22 08:42:53 +0000

    Initial Commit

---
+++ TestApplication1/app/src/main/java/org/me/gcu/labstuff/MainActivity.java
@@ -0,0 +1,14 @@
+package org.me.gcu.labstuff;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
